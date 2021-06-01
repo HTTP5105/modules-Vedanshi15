75dd36fde3aec8da84a3c4025db598837733d046
Vedanshi15 <78773515+Vedanshi15@users.noreply.github.com>
2021-06-01 11:06:45 -0400

    Updated

--- public/another-module.js
+++ public/another-module.js
@@ -1,11 +1,11 @@
 const HobbitSettlement = class {
-	constructor(name, founding, founders) {
+	constructor( name, founding, founders ) {
 		this.name = name;
 		this.founding = founding;
 		this.founders = founders;
 		this.originalSettlement = { name: 'Eriador', details: 'eriadorDetails' };
 	}
 };
-const theShire = new HobbitSettlement('The Shire', 1601, ['Marcho', 'Blanco']);
-const buckland = new HobbitSettlement('Buckland', 2340, 'Gorhendad Oldbuck');
-export { theShire, buckland, HobbitSettlement};
+const theShire = new HobbitSettlement( 'The Shire', 1601, ['Marcho', 'Blanco'] );
+const buckland = new HobbitSettlement( 'Buckland', 2340, 'Gorhendad Oldbuck' );
+export { theShire, buckland, HobbitSettlement };
