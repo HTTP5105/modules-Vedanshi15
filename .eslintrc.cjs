159d7d6a39fe8cc6c280176946922de6eb9cdfd2
Vedanshi15 <78773515+Vedanshi15@users.noreply.github.com>
2021-06-01 11:14:30 -0400

    Updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,8 @@ module.exports = {
     "no-tabs": 0,
     "indent": 0,
     "import/extensions": 0,
-    "no-unused-vars": 0
+    "no-unused-vars": 0,
+    "linebreak-style": 0,
+	  "space-in-parens": 0
   },
 };
